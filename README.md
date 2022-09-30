2b11c19f368a1573636f9cebfd792569c5b3c368
DESKTOP-0STHDG7\Administrator <fengzyworks@163.com>
2022-09-30 16:58:18 +0800

    monorepo 初始化

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # monorepo
 monorepo 测试项目
+
+
+https://www.jianshu.com/p/906a76b84332
---
+++ libs/core/index.js
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+export const testReq = ()=> axios.get('https://apis.juhe.cn/fapig/alarm/queryV2')
